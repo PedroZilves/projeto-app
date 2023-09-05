9e4bda949a10ae14b40535bcb22844ef289767f9
PedroZilves <rm99482@fiap.com.br>
2023-09-05 11:11:26 -0300

    importando home,produtos etc

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+//bloco de rotas
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produtos.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx'
+import Erro404 from './routes/Erro404.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
