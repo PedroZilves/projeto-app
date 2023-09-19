7b2fff8ecbcc1fb6444b7169c0e0e592e18f2a0b
PedroZilves <rm99482@fiap.com.br>
2023-09-19 10:43:56 -0300

    lista prod

---
+++ src/routes/ListaProdutosExterna.jsx
@@ -0,0 +1,10 @@
+//Nao é um componente
+
+export const ListaProdutosExterna = [
+
+{id:1,nome:"Teclado",desc:"Teclado 102 Teclas Mecânico.",preco:180},
+{id:2,nome:"Monitor",desc:"23'. 75Hz Tela curva",preco:2550},
+{id:3,nome:"Mouse óptioco",desc:"6 botoes",preco:220},
+{id:4,nome:"Mouse Pad",desc:"Mouse Pad Gamer 80X100",preco:100},
+{id:5,nome:"Headset",desc:"Head set gamer 8D",preco:800},
+];
