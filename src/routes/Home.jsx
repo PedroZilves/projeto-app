6da07345baa87445fed585611e5607ab7f672227
PedroZilves <rm99482@fiap.com.br>
2023-10-10 10:44:14 -0300

    imagem adicionada a home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div><h1>Home</h1>
+    <h2>Produtos em oferta.....</h2>
+    <figure><img src="img/produtos2.png" alt="Prateleira" /><figcaption>Nossos produtos</figcaption></figure></div>
   )
 }
