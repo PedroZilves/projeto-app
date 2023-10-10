56511ee085edc1e67f23f29f23ee6d9760c47bff
PedroZilves <rm99482@fiap.com.br>
2023-10-10 10:24:37 -0300

    amem

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 //BLOCO DE ROTAS
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
@@ -9,6 +8,7 @@ import Home from './routes/Home.jsx';
 import Produtos from './routes/Produtos.jsx';
 import EditarProdutos from './routes/EditarProdutos.jsx';
 import Erro404 from './routes/Erro404.jsx';
+import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
 
 const router = createBrowserRouter([ 
   {path: "/", element: <App/> , errorElement: <Erro404/>, 
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
       {path: "/", element: <Home/>},
       {path: "/produtos", element: <Produtos/>},
       {path: "/editar/produtos/:id", element: <EditarProdutos/>},
+      {path: "/excluir/produtos/:id", element: <ExcluirProdutos/>},
     ]
   }
 ]);
@@ -23,7 +24,5 @@ const router = createBrowserRouter([
 //BLOCO DE ROTAS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <RouterProvider router={router}/>
-  </React.StrictMode>,
 )
